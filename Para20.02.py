16069b5a08b138d3fe45fe2f69f06d73a06b9096
Yonlero184 <pedroggames12@gmail.com>
2024-08-18 23:21:21 -0300

    Add files via upload
    Sei lá

---
+++ Para20.02.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.color("red")
+tela = turtle.Screen()
+turtle.begin_fill()
+turtle.pensize(3)
+turtle.left(50)
+turtle.forward(133)
+turtle.circle(50,200)
+turtle.right(140)
+turtle.circle(50,200)
+turtle.forward(133)
+turtle.end_fill()
+tela.exitonclick()
